af80ed61f1cdb5aa6ef1f3b3d14adad834197243
rsRitik <ritiksingh12304@gmail.com>
2023-09-21 21:53:20 +0530

    Second git commit

--- src/calculator.java
+++ src/calculator.java
@@ -1,6 +1,9 @@
+import java.sql.SQLOutput;
+
 public class calculator {
     public static void main(String[] args) {
         System.out.println("hi");
         System.out.println("hehs");
+        System.out.println("hehe");
     }
 }
